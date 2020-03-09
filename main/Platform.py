96119b8412ae9d18420909bc284e33d10e80f718
Mykola <rucinikolay@gmail.com>
2020-03-09 15:24:26 +0200

    Check if Login page present before refreshing a page

--- main/Platform.py
+++ main/Platform.py
@@ -1,7 +1,10 @@
 import time
 
+from selenium.webdriver.common.by import By
 from main.pages.BasePage import BasePage
 from main.pages.LoginPage import LoginPage
+
+EMAIL_FIELD = (By.ID, "signinEmail")
 
 
 class Platform(BasePage):
@@ -12,7 +15,12 @@ class Platform(BasePage):
         print("Open '%s' application" % url)
         time.sleep(2)
         self.driver.get(url + "/login")
-        time.sleep(10)
-        self.driver.refresh()
         time.sleep(5)
+
+        try:
+            self.wait_element_present(EMAIL_FIELD).click()
+        except Exception:
+            self.driver.refresh()
+            time.sleep(5)
+
         return LoginPage(self.driver)
