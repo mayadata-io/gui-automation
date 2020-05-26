09168ae5c46326f3867b0713656019cac0e4f60e
rohan2794 <rohan.kumar@mayadata.io>
2020-05-26 22:53:39 +0530

    openebs components version check  test

---
+++ tests/openebs_test.py
@@ -0,0 +1,34 @@
+import allure
+import pytest
+
+from main.Platform import Platform
+from main.common.Utils import Utils
+from main.common import Config
+
+
+class TestOpenEbs:
+
+    # Complete OEP user profile which got created through API and verify OpenEbs components version
+    @pytest.mark.openebs
+    @allure.testcase("To verify profile completion for ORP user and OpenEbs components version check.")
+    def test_verify_openebs_components_version(self, driver, url):
+        prefix = Utils.random_string(6)
+        phone = Utils.random_number(10)
+        Platform(driver).launch(url) \
+            .login_as_oep_user("oep.user@mayadata.io", "OEPuser@123") \
+            .wait_onboarding_page_loaded() \
+            .verify_onboarding_page_title_equals("Update your profile") \
+            .enter_company_name(prefix + "Putbox") \
+            .enter_role(prefix + "Automation") \
+            .enter_phone_number(phone) \
+            .click_continue_button() \
+            .verify_onboarding_page_title_equals("We have created a project for you!") \
+            .enter_project_name("") \
+            .click_continue_button() \
+            .enter_cluster_name(prefix) \
+            .click_connect_button() \
+            .click_close_button() \
+            .open_clusters_page() \
+            .open_cluster_details("oep-cluster-cluster2", "Active") \
+            .open_ebs_page() \
+            .verify_openebs_components_version("1.10.0-ee-RC3")
