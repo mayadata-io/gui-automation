44bb32fc8658c730930cfa26888a148ceefb7812
Nikolay Rusinko <rucinikolay@gmail.com>
2020-05-31 16:11:01 +0300

    Update configure-grid.sh

--- resources/grid/configure-grid.sh
+++ resources/grid/configure-grid.sh
@@ -7,12 +7,10 @@ echo '#### AWS CONFIG ####'
 mkdir -p ~/.aws
 cp $AWS_CREDS ~/.aws/credentials
 sed 's|region = us-west-2|region = eu-north-1|' -i ~/.aws/config
-
+# tests_count=`find . -type f -name '*_test.py' -exec grep -e 'def test_' '{}' \; | wc -l`
 tests_count=30
 echo "Number of GUI test scripts: $tests_count"
 
-git clone https://$username:$password@github.com/mayadata-io/oep-e2e-aws.git
-cd oep-e2e-aws/stages/16-selenium-grid/templates
 cat selenium-grid-config.yml
 
 aws cloudformation create-stack --stack-name ${GUID} --template-body file://selenium-grid-config.yml --parameters ParameterKey=NumberOfChromeNodes,ParameterValue=$tests_count ParameterKey=ClusterName,ParameterValue=${GUID} ParameterKey=LogName,ParameterValue=${GUID}
