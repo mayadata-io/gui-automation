e6643e2cc943b20031e666c21f9efeeb46d2d826
rohan2794 <rohan.kumar@mayadata.io>
2020-06-26 22:25:58 +0530

    aws grid commented

--- scripts/dop-gui-stages/run-gui-tests.sh
+++ scripts/dop-gui-stages/run-gui-tests.sh
@@ -7,19 +7,20 @@ URL="$3"
 THREADS="$4"
 REGION="$5"
 
-echo '#### AWS CONFIG ####'
-if [[ $GUID == *"e2e-aws"* ]]; then
-	mkdir -p ~/.aws
-	cp $AWS_CREDS ~/.aws/credentials
-fi
-echo '#### Output AWS Selenium Grid stack ####'
-output=`aws cloudformation describe-stacks --stack-name $GUID --region $REGION --query Stacks[].Outputs[].OutputValue | sed -r 's/"+//g'`
-grid=`echo $output | awk {'print $2'}`
-echo 'Selenium Grid: ' $grid
+#echo '#### AWS CONFIG ####'
+#if [[ $GUID == *"e2e-aws"* ]]; then
+#	mkdir -p ~/.aws
+#	cp $AWS_CREDS ~/.aws/credentials
+#fi
+#echo '#### Output AWS Selenium Grid stack ####'
+#output=`aws cloudformation describe-stacks --stack-name $GUID --region $REGION --query Stacks[].Outputs[].OutputValue | sed -r 's/"+//g'`
+#grid=`echo $output | awk {'print $2'}`
+#echo 'Selenium Grid: ' $grid
 
 ######################
 ##   Running test  ##
 ######################
 python3.7 -m pip install -r requirements.txt
 #Running tests with apropriate marker
-python3.7 -m pytest -m $GROUP --url $URL --environment remote --hub $grid -v --tests-per-worker $THREADS --reruns 1 --html=./results/report.html
+#python3.7 -m pytest -m $GROUP --url $URL --environment remote --hub $grid -v --tests-per-worker $THREADS --reruns 1 --html=./results/report.html
+python3.7 -m pytest -m $GROUP --url $URL --environment remote -v --tests-per-worker $THREADS --reruns 1 --html=./results/report.html
