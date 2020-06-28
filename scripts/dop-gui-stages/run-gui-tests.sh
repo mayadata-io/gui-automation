107af1078da83eef33bbdfc9405ee056cd01ebdf
rohan2794 <rohan.kumar@mayadata.io>
2020-06-28 13:32:04 +0530

    test grid setup

--- scripts/dop-gui-stages/run-gui-tests.sh
+++ scripts/dop-gui-stages/run-gui-tests.sh
@@ -23,4 +23,14 @@ REGION="$5"
 python3.7 -m pip install -r requirements.txt
 #Running tests with apropriate marker
 #python3.7 -m pytest -m $GROUP --url $URL --environment remote --hub $grid -v --tests-per-worker $THREADS --reruns 1 --html=./results/report.html
+
+# Test grid connection
+echo '#### curl http://106.51.78.18:6030 ####'
+CURL_EXT=`curl http://106.51.78.18:6030`
+echo $CURL_EXT
+
+echo '#### curl http://10.66.2.5:4444 ####'
+CURL_INT=`curl http://10.66.2.5:4444`
+echo $CURL_CURL_INT
+
 python3.7 -m pytest -m $GROUP --url $URL --environment remote -v --tests-per-worker $THREADS --reruns 1 --html=./results/report.html
