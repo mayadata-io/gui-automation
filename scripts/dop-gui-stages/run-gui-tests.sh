88b584c045335e079aebef609344550f79d0ad9d
rohan2794 <rohan.kumar@mayadata.io>
2020-06-30 16:47:29 +0530

    konvoy grid updated

--- scripts/dop-gui-stages/run-gui-tests.sh
+++ scripts/dop-gui-stages/run-gui-tests.sh
@@ -7,7 +7,6 @@ URL="$3"
 THREADS="$4"
 REGION="$5"
 
-
 if [[ $GUID == *"e2e-aws"* ]]; then
   echo '#### AWS CONFIG ####'
 	mkdir -p ~/.aws
@@ -28,7 +27,7 @@ if [[ $GUID == *"e2e-konvoy"* ]]; then
   # Test grid connection
   echo '#### curl http://10.66.2.5:4444 ####'
   CURL_INT=`curl http://10.66.2.5:4444`
-  echo $CURL_CURL_INT
+  echo $CURL_INT
   echo '#### Output konvoy Selenium Grid stack ####'
   grid="10.66.2.5"
   echo 'Selenium Grid: ' $grid
@@ -39,16 +38,15 @@ if [[ $GUID == *"e2e-konvoy"* ]]; then
   python3.7 -m pytest -m $GROUP --url $URL --environment remote --hub $grid -v --tests-per-worker $THREADS --reruns 1 --html=./results/report.html
 fi
 if [[ $GUID == *"e2e-rancher"* ]]; then
-  # Test grid connection
-  echo '#### curl http://10.66.2.5:4444 ####'
-  CURL_INT=`curl http://10.66.2.5:4444`
-  echo $CURL_CURL_INT
-  echo '#### Output konvoy Selenium Grid stack ####'
-  grid="10.66.2.5"
+  echo '#### Output AWS Selenium Grid stack ####'
+  output=`aws cloudformation describe-stacks --stack-name $GUID --region $REGION --query Stacks[].Outputs[].OutputValue | sed -r 's/"+//g'`
+  grid=`echo $output | awk {'print $2'}`
   echo 'Selenium Grid: ' $grid
+
   ######################
   ##   Running test  ##
   ######################
+  python3.7 -m pip install -r requirements.txt
   #Running tests with apropriate marker
   python3.7 -m pytest -m $GROUP --url $URL --environment remote --hub $grid -v --tests-per-worker $THREADS --reruns 1 --html=./results/report.html
 fi
